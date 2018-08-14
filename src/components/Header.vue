6f573e148d973abdd422f73e0cd3a9f0b8f09fb6
tom lee <tdroid99@gmail.com>
2018-08-14 15:53:22 -0600

    added header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,21 @@
 <template>
-    <header>
-        <h1>Hello</h1>
-    </header>
+    
+        <b-jumbotron center bg-variant="info" text-variant="white" border-variant="dark">
+    <template slot="header">
+    Safe-Space
+    </template>
+</b-jumbotron>
+    
 </template>
 
 <script>
-export default {}; 
+export default {};
 </script>
 
 <style>
-
+header {
+    display: flex;
+    align-content: center;
+    width: 100%;
+}
 </style>
