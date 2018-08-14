1b02260fbb996758bbf9a3bfdded4e7ec070aabd
tricks7 <tricks7@me.com>
2018-08-14 11:31:20 -0600

    added vue-bootstrap and dependencies

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <router />
+  </div>
+</template>
+
+<script>
+import Form from './components/Form'
+
+export default {
+  name: 'App',
+  components: {
+    Form
+  },
+  data(){
+    return {
+      teaFlavor: 'Mint'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
