eceb8a102ee7ab92c236d9a3983b20d88543ad06
tom lee <tdroid99@gmail.com>
2018-08-15 10:53:17 -0600

    added footer card

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+<b-card-footer>
+    <small>2018</small>
+    <h6>All post are property and opionion of poster and shoudld not be considered legal and professional advice.</h6>
+</b-card-footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    footer {
+        /* background-color: #00A5BB; */
+        display: flex;
+        flex-direction: column-reverse;
+        align-content: space-between;
+    }
+</style>
+
