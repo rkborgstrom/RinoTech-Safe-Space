7c2e8637d67555fc99fedf48eaf9eccb3967b510
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-08-16 13:41:00 -0600

    getting closer to posting cards

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,35 +5,36 @@
     </section>
     <b-modal id="modal1" title="What are you going through?">
       <b-form v-on:submit.prevent='onSubmit' v-on:submit="resetForm">
+        {{post.title}}
         <input v-model="title" name="title" required placeholder="Title">
-
-      <br />
-      <div>
-        <select v-model="topics" name="topics">
-              <option disabled value="">Topics</option>
-              <option>Depression</option>
-              <option>Anxiety</option>
-              <option>Stress</option>
-              <option>LGBT</option>
-              <option>PTSD</option>
-              <option>Alcohol</option>
-              <option>Marital</option>
-              <option>Death</option>
-              <option>Loneliness</option>
-              <option>Family Issues</option>
-              <option>Other</option>
-          </select>
+    
         <br />
+        <div>
+          <select v-model="topics" name="topics">
+                <option disabled value="">Topics</option>
+                <option>Depression</option>
+                <option>Anxiety</option>
+                <option>Stress</option>
+                <option>LGBT</option>
+                <option>PTSD</option>
+                <option>Alcohol</option>
+                <option>Marital</option>
+                <option>Death</option>
+                <option>Loneliness</option>
+                <option>Family Issues</option>
+                <option>Other</option>
+            </select>
+          <br />
+          <br />
+          <span><strong>Selected:</strong> {{ selected }}</span>
+        </div>
         <br />
-        <span><strong>Selected:</strong> {{ selected }}</span>
-      </div>
-      <br />
-      <div>
-        <b-form-textarea id="textarea2" v-model="story" name="story" placeholder="Tell us about it..." :rows="3">
-        </b-form-textarea>
-        <h6>We keep posts anonymous</h6>
-      </div>
-      <b-button name="submit" value="submit" type="submit" variant="success">Submit</b-button>
+        <div>
+          <b-form-textarea id="textarea2" v-model="story" name="story" placeholder="Tell us about it..." :rows="3">
+          </b-form-textarea>
+          <h6>We keep posts anonymous</h6>
+        </div>
+        <b-button name="submit" value="submit" type="submit" variant="success">Submit</b-button>
       </b-form>
     </b-modal>
   </div>
@@ -41,8 +42,12 @@
 
 <script>
   export default {
+    props: ['post'],
+    name: "Modal",
     data() {
       return {
+        posts: [],
+        apiURL: "https://rinotech-safe-space.herokuapp.com/posts",
         text: null,
         selected: null,
         title: null,
@@ -50,6 +55,21 @@
         story: null
       }
     },
+    mounted() {
+      fetch(this.apiURL, {
+          method: 'get',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(res => res.json())
+        .then(data => {
+          this.posts = data
+          console.log(this.posts)
+        })
+    },
+
     methods: {
       onSubmit() {
         console.log('hello')
@@ -62,12 +82,11 @@
         this.$emit('addPost', newPost)
       },
       resetForm() {
-        this.title=null;
-        this.topics=null;
-        this.story=null;
+        this.title = null;
+        this.topics = null;
+        this.story = null;
       }
     },
-    props: ['post']
   }
 </script>
 
@@ -100,10 +119,31 @@
   .btn-success {
     background-color: #ffdc36;
   }
-
 </style>
 
 
 
 
+  data(){
+    return {
 
+    posts: [],
+    apiURL: "https://rinotech-safe-space.herokuapp.com/posts"
+      }
+    },
+  
+    mounted() {
+      fetch(this.apiURL, {
+          method: 'get',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(res => res.json())
+        .then(data => {
+          this.posts = data
+          console.log(this.posts)
+        })
+    }
+  }
