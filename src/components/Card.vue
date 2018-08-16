6d34ef6f536e6c6a2f6e3c2da4754b4237a48535
rusticpenguin <georgechios.music@gmail.com>
2018-08-16 15:44:05 -0600

    Karma

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,21 +4,27 @@
       <b-container class="bv-example-row">
         <b-row>
           <b-col>
-            <h2>{{post.title}}</h2>
+            <h2 class="postTitle">{{post.title}}</h2>
           </b-col>
           <b-col>
             <h4 class="topic">Topic: {{post.topic}}</h4>
           </b-col>
         </b-row>
       </b-container>
+              <hr>
       <p class="card-text pl-5 pr-5">Message: {{post.story}}</p>
+              <hr/>
       <b-container>
         <b-row>
           <b-col>
-            <h6>Karma Score: {{post.karma}}</h6>
+            <h3 class="karma" v-on:click="counter += 1"> + </h3>
+              <h6>Karma Score: {{counter}}</h6>
+            <h3 class="karma" v-on:click="counter -= 1"> - </h3>
           </b-col>
           <b-col>
+          <br>
             <h6 class="topic"> Data Posted: {{post.date}}</h6>
+            <br>
           </b-col>
         </b-row>
       </b-container>
@@ -43,6 +49,14 @@
     data() {
       return {
         text: '',
+        counter: 0
+      }
+    },
+    update: {
+      deleteCard(){
+        if (counter <= -5){
+          console.log('hi')
+        }
       }
     },
     props: ['post']
@@ -69,5 +83,14 @@
     color: black;
     background: linear-gradient(90deg, rgba(255, 185, 28, 1) 0%, rgba(255, 221, 58, 1) 50%, rgba(255, 215, 45, 1) 100%);
     ;
+  }
+
+  .karma {
+    display: flex;
+    padding-left: 50px;
+  }
+
+  .postTitle {
+    padding-left: 6%;
   }
 </style>
